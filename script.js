ccc35e2524b016e0c1d3545e43c34a2f2b9c35fa
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-07-17 11:09:07 +0200

    [UPD] Bonus 2 completato

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-
 async function getChefBirthday(id) {
     try {
         const response = await fetch(`https://dummyjson.com/recipes/${id}`);
         const obj = await response.json();
         const response2 = await fetch(`https://dummyjson.com/users/${obj.userId}`)
         const birthday = await response2.json();
-        return birthday
+        const dateBirthday = dayjs(birthday.birthDate).format("MM-DD-YYYY")
+        return dateBirthday
     } catch (error) {
         console.error(error)
     }
-
-
 }
 
 // esempi di utilizzo
 (async () => {
     try {
         const birthday = await getChefBirthday(20);
-        console.log('Ecco la data di nascita dello chef:', birthday.birthDate)
+        console.log('Ecco la data di nascita dello chef:', birthday);
     } catch (error) {
         console.error(error);
     }
